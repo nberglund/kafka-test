72d907cada1be708853c6fb1ff48d0ead1708564
Niels Berglund <niels.it.berglund@gmail.com>
2014-12-23 21:28:36 +0200

    first code snippet with KafkaNet

--- cs/KafkaTest1/KafkaTest1/Program.cs
+++ cs/KafkaTest1/KafkaTest1/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
 
 namespace KafkaTest1
 {
@@ -10,6 +13,18 @@ namespace KafkaTest1
   {
     static void Main(string[] args)
     {
+      var options = new KafkaOptions(new Uri("http://localhost:9092"));
+      var router = new BrokerRouter(options);
+      var client = new Producer(router);
+
+      var message = "Hello Kafka from C# - 2";
+
+      client.SendMessageAsync("TestHarness", new[] { new Message(message) }).Wait();
+
+      using (client) { }
+
+
+
     }
   }
 }
